3a681b2578757913f31f8e9c923f230f732d8df8
Lemoncchi <mr.nobody.lim.l@gmail.com>
2023-10-16 23:08:39 +0800

    完成 nowcoder_nickname_crawler 和 vjudge_nickname_crawler 的 aync

--- utils/nowcoder_nickname_crawler.py
+++ utils/nowcoder_nickname_crawler.py
@@ -1,35 +1,40 @@
+import asyncio
 import re
 from urllib import parse
 
+import aiohttp
 import requests
 
 import config
 
 
-def get_nowcoder_nickname(url: str) -> str | None:
+async def get_nowcoder_nickname(url: str, session: aiohttp.ClientSession) -> str | None:
     domain = parse.urlparse(url).netloc
     if domain == "www.nowcoder.com":
         nowcoder_id = url.split("/")[-1]
         url = f"https://ac.nowcoder.com/acm/contest/profile/{nowcoder_id}"
 
-    response = requests.get(url)
+    async with session.get(url) as response:
+        re_nickname = re.compile(r'data-title="(.*)"', re.M | re.I)
 
-    re_nickname = re.compile(r'data-title="(.*)"', re.M | re.I)
-    matchObj = re_nickname.search(response.text)
+        matchObj = re_nickname.search(await response.text())
 
-    if matchObj is not None:
-        return matchObj.group(1).strip()
-    else:
-        
-        return None
+        if matchObj is not None:
+            return matchObj.group(1).strip()
+        else:
+            return None
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        nick_name = await get_nowcoder_nickname(
+            "https://ac.nowcoder.com/acm/contest/profile/767116230", session
+        )
+        assert nick_name == "lim_Nobody"
+        user_url = "https://www.nowcoder.com/users/804688108"  # 非 contest profile
+        nick_name = await get_nowcoder_nickname(user_url, session)
+        print(nick_name)
 
 
 if __name__ == "__main__":
-    nick_name = get_nowcoder_nickname(
-        "https://ac.nowcoder.com/acm/contest/profile/767116230"
-    )
-    print(nick_name)
-    assert nick_name == "lim_Nobody"
-    user_url = "https://www.nowcoder.com/users/804688108"  # 非 contest profile
-    nick_name = get_nowcoder_nickname(user_url)
-    print(nick_name)
+    asyncio.run(main())
