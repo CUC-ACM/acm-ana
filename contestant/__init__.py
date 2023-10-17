9951a0623e3abc9df2f5d05f686a40d161471134
Lemoncchi <mr.nobody.lim.l@gmail.com>
2023-10-17 18:03:06 +0800

    新增 username UNIQUE 限制

--- contestant/__init__.py
+++ contestant/__init__.py
@@ -11,7 +11,7 @@ class ContestantBase:
     real_name: Mapped[Optional[str]] = mapped_column(String())
     student_id: Mapped[Optional[str]] = mapped_column(String(), unique=True)
     nickname: Mapped[Optional[str]] = mapped_column(String())
-    username: Mapped[str] = mapped_column(String())
+    username: Mapped[str] = mapped_column(String(), unique=True)
     is_in_course: Mapped[bool] = mapped_column()  # 是否在选课名单中
     major: Mapped[Optional[str]] = mapped_column(String())
     grade: Mapped[Optional[str]] = mapped_column(Integer())
