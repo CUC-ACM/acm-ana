0137543dd670195120edfcd33b19ba9cc37a241a
Lemoncchi <mr.nobody.lim.l@gmail.com>
2023-10-17 11:13:17 +0800

    将 contest 中的 name 字段改为 title

--- contest/__init__.py
+++ contest/__init__.py
@@ -11,7 +11,7 @@ class ContestBase:
     """比赛元信息的基类"""
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String())  # 比赛名称
+    title: Mapped[str] = mapped_column(String())  # 比赛名称
     start_time: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=False))
     end_time: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=False))
     url: Mapped[Optional[str]] = mapped_column(String())
