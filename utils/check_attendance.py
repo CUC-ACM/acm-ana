3f236d3e5a245c4ed22a8be9f542e296eca8212c
Lemoncchi <mr.nobody.lim.l@gmail.com>
2023-10-17 09:40:17 +0800

    新增检查选课人员是否在数据库中

---
+++ utils/check_attendance.py
@@ -0,0 +1,29 @@
+import logging
+
+import pandas as pd
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+import config
+from contestant.nowcoder_contestant import NowcoderContestant
+
+logger = logging.getLogger(__name__)
+
+
+def check_attendance():
+    """检查是否有已经选课的同学没有填写问卷"""
+    with Session(config.engine) as sqlsession:
+        df = pd.read_csv(config.config["input"]["attendance_path"])
+        for index, row in df.iterrows():
+            student_id = row["学号"]
+            stmt = select(NowcoderContestant).where(
+                NowcoderContestant.student_id == student_id
+            )
+            cached_nowcoder_contestant = sqlsession.execute(stmt).scalar_one_or_none()
+
+            if not cached_nowcoder_contestant:
+                logging.info(f"已选课未填表: {row['姓名']}")
+
+
+if __name__ == "__main__":
+    check_attendance()
