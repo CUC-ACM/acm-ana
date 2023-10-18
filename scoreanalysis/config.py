aa3af91d2de59f0ecc12dddd495f053230c9d844
Lemoncchi <mr.nobody.lim.l@gmail.com>
2023-10-18 16:12:33 +0800

    重构 config 文件位置

--- scoreanalysis/config.py
+++ scoreanalysis/config.py
@@ -1,19 +1,6 @@
 import logging.config
 
 import yaml
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from contest.nowcoder_contest import NowcoderContest
-from contest.vjudge_contest import VjudgeContest
-from contestant.nowcoder_contestant import NowcoderContestant
-from contestant.vjudge_contestant import VjudgeContestant
-from ranking.nowcoder_ranking import NowcoderRanking
-from ranking.vjudge_ranking import VjudgeRanking
-
-engine = create_engine("sqlite:///data/sqlite_data.db", echo=False)
-SessionMaker = sessionmaker(bind=engine)
-sqlsession = SessionMaker()
 
 with open("config.yaml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
