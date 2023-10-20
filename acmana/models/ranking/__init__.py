ae6964d63b2de0110e8c18ebefc40cd7799d0d63
Lemoncchi <mr.nobody.lim.l@gmail.com>
2023-10-20 15:08:35 +0800

    修正数据库没有参加比赛的同学的 competition_rank 为 None

--- acmana/models/ranking/__init__.py
+++ acmana/models/ranking/__init__.py
@@ -12,7 +12,7 @@ class RankingBase:
     """排名信息的基类(各场比赛混在一起)"""
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    competition_rank: Mapped[int] = mapped_column(Integer())
+    competition_rank: Mapped[int | None] = mapped_column(Integer())
     score: Mapped[float] = mapped_column(Float())
     solved_cnt: Mapped[int] = mapped_column(Integer())
     upsolved_cnt: Mapped[int] = mapped_column(Integer())  # 补题数
