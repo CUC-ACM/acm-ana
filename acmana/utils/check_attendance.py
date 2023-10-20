f0a5fdf9de442bcc436034ec5091a6d761034d5e
Lemoncchi <mr.nobody.lim.l@gmail.com>
2023-10-20 09:38:33 +0800

    修复重构 check_attendance

--- acmana/utils/check_attendance.py
+++ acmana/utils/check_attendance.py
@@ -3,23 +3,22 @@ import logging
 import pandas as pd
 
 import acmana
-from acmana.models.account.nowcoder_account import NowcoderAccount
+from acmana.models.student import Student
 
 logger = logging.getLogger(__name__)
 
 
-def check_attendance():
-    """检查是否有已经选课的同学没有填写问卷"""
-
+def check_attendance() -> list[str]:
+    """检查是否有已经选课的同学没有填写问卷，返回没有填写同学的姓名列表"""
+    not_fill_questionnaire: list[str] = []
     df = pd.read_csv(acmana.config["input"]["attendance_path"])
     for index, row in df.iterrows():
         student_id = row["学号"]
-        cached_nowcoder_account = NowcoderAccount.query_from_student_id(
-            student_id=student_id
-        )
-
-        if not cached_nowcoder_account:
-            logging.info(f"已选课未填表: {row['姓名']}")
+        db_student = Student.query_from_student_id(student_id)
+        if db_student is None:
+            logger.warning(f"已选课未填写问卷: {row['姓名']}")
+            not_fill_questionnaire.append(row["姓名"])
+    return not_fill_questionnaire
 
 
 if __name__ == "__main__":
