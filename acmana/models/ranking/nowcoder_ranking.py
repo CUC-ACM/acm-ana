aa49c970c415e8a71145c826604c89728e84f7d5
Lemoncchi <mr.nobody.lim.l@gmail.com>
2023-10-20 10:55:31 +0800

    为了防止重复提交，这里设置了唯一索引，确保同一个账号只能在 同一个比赛中出现在 vjudge_ranking & nowcoder_ranking  中一次

--- acmana/models/ranking/nowcoder_ranking.py
+++ acmana/models/ranking/nowcoder_ranking.py
@@ -1,14 +1,14 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, Index
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from acmana.models import SQLBase
 from acmana.models.ranking import RankingBase
 
 if TYPE_CHECKING:
-    from acmana.models.contest.nowcoder_contest import NowcoderContest
     from acmana.models.account.nowcoder_account import NowcoderAccount
+    from acmana.models.contest.nowcoder_contest import NowcoderContest
 
 
 class NowcoderRanking(RankingBase, SQLBase):
@@ -19,3 +19,12 @@ class NowcoderRanking(RankingBase, SQLBase):
     account: Mapped["NowcoderAccount"] = relationship(back_populates="rankings")
     contest_id: Mapped[int] = mapped_column(ForeignKey("nowcoder_contest.id"))
     contest: Mapped["NowcoderContest"] = relationship(back_populates="rankings")
+
+
+# 为了防止重复提交，这里设置了唯一索引，确保同一个账号只能在同一个比赛中出现在 nowcoder_ranking 中一次
+Index(
+    "unique_nowcoder_account_contest",
+    NowcoderRanking.account_id,
+    NowcoderRanking.contest_id,
+    unique=True,
+)
--- acmana/models/ranking/vjudge_ranking.py
+++ acmana/models/ranking/vjudge_ranking.py
@@ -1,20 +1,21 @@
 from typing import TYPE_CHECKING, Sequence
 
-from sqlalchemy import ForeignKey, Select
+from sqlalchemy import ForeignKey, Index, Select
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from acmana.models import SQLBase, sqlsession
 from acmana.models.ranking import RankingBase
 
 if TYPE_CHECKING:
-    from acmana.models.contest.vjudge_contest import VjudgeContest
     from acmana.models.account.vjudge_account import VjudgeAccount
+    from acmana.models.contest.vjudge_contest import VjudgeContest
 
 
 class VjudgeRanking(RankingBase, SQLBase):
     """存储 vjudge 所有比赛排名 的表(各场比赛混在一起)"""
 
     __tablename__ = "vjudge_ranking"
+
     account_id: Mapped[int] = mapped_column(ForeignKey("vjudge_account.id"))
     account: Mapped["VjudgeAccount"] = relationship(back_populates="rankings")
     contest_id: Mapped[int] = mapped_column(ForeignKey("vjudge_contest.id"))
@@ -33,3 +34,12 @@ class VjudgeRanking(RankingBase, SQLBase):
         if account_id is not None:
             stmt = stmt.where(cls.account_id == account_id)
         return sqlsession.execute(stmt).scalars().all()
+
+
+# 为了防止重复提交，这里设置了唯一索引，确保同一个账号只能在同一个比赛中出现在 vjudge_ranking 中一次
+Index(
+    "unique_vj_account_contest",
+    VjudgeRanking.account_id,
+    VjudgeRanking.contest_id,
+    unique=True,
+)
