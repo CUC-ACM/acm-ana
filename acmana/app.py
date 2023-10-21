5798ad8ed29b6fa0ecbefe77491dae6d14bfac82
Lemoncchi <mr.nobody.lim.l@gmail.com>
2023-10-21 22:45:47 +0800

    完成主函数的设计

--- acmana/app.py
+++ acmana/app.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import acmana
 from acmana.crawler.vjudge.contest import VjudgeContestCrawler
@@ -10,26 +11,41 @@ logger = logging.getLogger(__name__)
 
 
 def retrive_vjudge_contests():
-    for instance in acmana.config["vjudge"]["instances"]:
-        logger.info(
-            f"Retriving {instance['div']} contests from '{instance['title_prefix']}'......"
-        )
-        retriever = VjudgeContestRetriever(
-            title=instance["title_prefix"], div=instance["div"]
-        )
+    for div, instance in dict(acmana.config["vjudge"]["instances"]).items():
+        logger.info(f"Retriving {div} contests from '{instance['title_prefix']}'......")
+        retriever = VjudgeContestRetriever(title=instance["title_prefix"], div=div)
         retriever.get_contests_and_commit_to_db()
         for contest in retriever.retrieved_contests:
             logger.info(f"Retriving {contest} rank......")
-            vjudge_contest_crawler = VjudgeContestCrawler(
-                contest.id, div=instance["div"]
-            )
+            vjudge_contest_crawler = VjudgeContestCrawler(contest.id, div=div)
             vjudge_contest_crawler.db_vjudge_contest.commit_to_db()
 
 
 def export_vjudge_contests_to_excel():
     """将 `数据库` 中的 VjudgeContest 导出到 Excel 文件中"""
-    excel_book = ExcelBook()
-    excel_book.write_book()
+    for div, instance in dict(acmana.config["vjudge"]["instances"]).items():
+        logger.info(f"Exporting {div} contests from '{instance['title_prefix']}'......")
+        total_excel_file_path: str = os.path.join(
+            "outputs", instance["export_filename"] + "(全部同学).xlsx"
+        )
+        total_excel_book = ExcelBook(
+            path=total_excel_file_path,
+            div=div,
+            only_attendance=False,
+            sheet_name_remover=instance["sheet_name_remover"],
+        )
+        total_excel_book.write_book()
+
+        attendance_excel_file_path: str = os.path.join(
+            "outputs", instance["export_filename"] + "(选课同学).xlsx"
+        )
+        attendance_excel_book = ExcelBook(
+            path=attendance_excel_file_path,
+            div=div,
+            only_attendance=True,
+            sheet_name_remover=instance["sheet_name_remover"],
+        )
+        attendance_excel_book.write_book()
 
 
 def run():
